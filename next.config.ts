c47bf2472204481545a69134c0c2ccbc2890f39b
이성원 <57swji@gmail.com>
2026-08-30 18:25:39 +0900

    (config): ⚙️ SVG 아이콘 임포트 인프라와 이미지 원격 도메인 허용 추가
    Turbopack rules로 @svgr/webpack을 연결해 .svg를 리액트 컴포넌트로
    바로 import할 수 있게 하고, 카카오·알라딘 책 표지 이미지 도메인을
    next/image remotePatterns에 추가함. dev 서버 포트도 4000으로 고정
    (카카오 리다이렉트 URI와 맞춰야 해서).

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "search1.kakaocdn.net",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.aladin.co.kr",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as FemaleIcon } from "./female.svg";
+export { default as MaleIcon } from "./male.svg";
+export { default as KakaoIcon } from "./kakao.svg";
