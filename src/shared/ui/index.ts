02dab4eae04ef95a185a35cfb54bd0c40b2b4600
이성원 <57swji@gmail.com>
2026-09-01 04:06:58 +0900

    (shared/ui): ✨ RadioGroup 컴포넌트 추가
    base-ui radio-group을 Checkbox/Switch와 동일한 규격(포커스 링, 터치
    타깃, disabled 처리)으로 감쌌다.

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -9,6 +9,7 @@ export * from "./input";
 export * from "./label";
 export * from "./password-input";
 export * from "./popover";
+export * from "./radio-group";
 export * from "./separator";
 export * from "./skeleton";
 export * from "./switch";
